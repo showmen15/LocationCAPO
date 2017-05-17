ce97d7dab53110bfad03d4a2dd165df4a858ed24
showmen15 <showmen15@poczta.onet.pl>
2017-05-17 13:39:52 +0200

    protobuf projekt lokalizacji

---
+++ ArucoLocation/ArucoLocation.cpp
@@ -0,0 +1,26 @@
+// ArucoLocation.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include "ArucoLocation.pb.h"
+
+
+using namespace std;
+
+// protobuf
+//https://www.youtube.com/watch?v=x-u-hEFhDDo
+
+int main()
+{
+	Aruco::ArucoLocation testowy[10];
+
+
+
+	//testowy.set_alfa(45);
+
+	//cout << testowy.alfa();
+
+
+	return 0;
+}
+
