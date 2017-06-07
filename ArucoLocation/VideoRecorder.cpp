fa501790300654d7551c8a839b16aa80d2103cc8
szsz <showmen15@poczta.onet.pl>
2017-06-07 19:44:07 +0200

    videorecorder

--- ArucoLocation/VideoRecorder.cpp
+++ ArucoLocation/VideoRecorder.cpp
@@ -11,8 +11,43 @@ VideoRecorder::~VideoRecorder()
 }
 
 
-VideoRecorder::StartRecord(string sFileName)
+void VideoRecorder::StartRecord(string sFileName)
 {
 }
+
+
+void VideoRecorder::init()
+{
+	//outputVideo = new VideoWriter();
+
+	//outputVideo = new VideoWriter();
+	//
+	//// Open the output
+ //   if (askOutputType)
+ //       outputVideo.open(NAME, ex=-1, inputVideo.get(CV_CAP_PROP_FPS), S, true);
+ //   else
+ //       outputVideo.open(NAME, ex, inputVideo.get(CV_CAP_PROP_FPS), S, true);
+
+ //   if (!outputVideo.isOpened())
+ //   {
+ //       cout  << "Could not open the output video for write: " << source << endl;
+ //       return -1;
+ //   }
+}
+
+void VideoRecorder::StartRecord(string sFileName)
+{
+}
+
+void VideoRecorder::Record(Mat frame)
+{
+}
+
+void VideoRecorder::EndRecord()
+{
+	outputVideo->release();
+	delete outputVideo;
+}
+
 
 
--- ArucoLocation/VideoRecorder.h
+++ ArucoLocation/VideoRecorder.h
@@ -2,15 +2,29 @@
 #define VIDEORECORDER_H_
 
 #include <iostream>
+#include <string>
+
+#include <opencv2/core/core.hpp>        // Basic OpenCV structures (cv::Mat)
+#include <opencv2/highgui/highgui.hpp>  // Video write
+
+using namespace std;
+using namespace cv;
 
 
 class VideoRecorder
 {
+private:
+	void init();
+	VideoWriter* outputVideo; 
+	string zmienna;
+
 public:
 	VideoRecorder(void);
 	~VideoRecorder(void);
 
-	StartRecord(string sFileName);
+	void StartRecord(string sFileName);
+	void Record(Mat frame);
+	void EndRecord();
 };
 
 #endif /* VIDEORECORDER_H_ */
